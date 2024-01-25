e6b80a2f83097b31f57a21d3f91d0c7b3bb40f20
MoiZP99 <eliaszp2501@gmail.com>
2024-01-24 22:45:12 -0600

    fin

--- includes/database.php
+++ includes/database.php
@@ -11,7 +11,7 @@ function conectarDB() : mysqli {
     $db->set_charset('utf8');
 
     if(!$db) {
-        echo "Error no se pudo conectar";
+        echo "Error no se pudo conectar al sistema";
         exit;
     } 
 
